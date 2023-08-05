22e860ff4aead0d0172b0ac6c2ee989c02058805
Prafful Patel <110104505+Prafful2003@users.noreply.github.com>
2023-08-04 19:32:57 -0700

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        string temp="";
+        for (int i=0; i<n; i++)
+        {
+            if (names[i].length() > temp.length())
+              temp= names[i];
+        }
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
