1d34690473e4c05ea51a8e9b65aa0a97e9af5027
Prafful Patel <110104505+PraffulPatel27@users.noreply.github.com>
2024-09-09 00:09:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+          int mid=0;
+        int min=0;
+        int max= arr.size()-1;
+        while(mid<=max){
+            if(arr[mid]==0){
+                swap(arr[mid++], arr[min++]);
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr[mid], arr[max--]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
