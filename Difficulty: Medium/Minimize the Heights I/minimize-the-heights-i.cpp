c25bff7448b89b8412a45d4911492cbee52309f5
Prafful Patel <110104505+PraffulPatel27@users.noreply.github.com>
2024-11-23 13:09:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int k, vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        if (n == 1) return 0; // Single tower, no difference
+
+        // Sort the array
+        sort(arr.begin(), arr.end());
+
+        // Initial difference
+        int diff = arr[n - 1] - arr[0];
+
+        // Initial smallest and largest values
+        int smallest = arr[0] + k;
+        int largest = arr[n - 1] - k;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minTower = min(smallest, arr[i + 1] - k);
+            int maxTower = max(largest, arr[i] + k);
+
+            // Update the minimum difference
+            diff = min(diff, maxTower - minTower);
+        }
+
+        return diff;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        int k;
+        cin >> k;
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int res = ob.getMinDiff(k, arr);
+        cout << res;
+
+        cout << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
